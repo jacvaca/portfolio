5a6fabd3729b732ec3171527f5fda301c839bfe6
jacq vaca <94397829+jacvaca@users.noreply.github.com>
2025-06-25 18:45:21 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App"; // <-- Make sure this path matches your file name and location
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
