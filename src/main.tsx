8bf2f2c4c79052599c52b0c60c916b8b5b3cb886
jacq vaca <94397829+jacvaca@users.noreply.github.com>
2025-06-25 18:43:00 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App"; // <-- Make sure this path matches your file name and location
 
-<BrowserRouter basename="/portfolio/">
-  <App />
-</BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/portfolio">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
